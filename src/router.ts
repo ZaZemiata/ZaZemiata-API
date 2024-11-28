d2f1ac8d79cf95da1db2f13391ee16eed824a571
Daniel <danieldimitrov2304@gmail.com>
2024-11-28 12:19:44 +0200

    Revert "Add route for crawledData controller"
    This reverts commit 96c0df476c51a07d7b951be13e29ad86d4b65cbe.

--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,6 @@
 import express from "express"
 import keyWordsController from "./controllers/keyWordsController"
 import sourceController from "./controllers/sourceController"
-import crawledDataController from "./controllers/crawledDataController"
 
 //Crete a router
 const router = express.Router();
@@ -18,7 +17,6 @@ const router = express.Router();
 //Add controllers to router
 router.use(keyWordsController)
 router.use(sourceController)
-router.use(crawledDataController)
 
 //Export a router
 export default router
