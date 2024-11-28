96c0df476c51a07d7b951be13e29ad86d4b65cbe
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-11-28 12:09:46 +0200

    Add route for crawledData controller

--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,7 @@
 import express from "express"
 import keyWordsController from "./controllers/keyWordsController"
 import sourceController from "./controllers/sourceController"
+import crawledDataController from "./controllers/crawledDataController"
 
 //Crete a router
 const router = express.Router();
@@ -17,6 +18,7 @@ const router = express.Router();
 //Add controllers to router
 router.use(keyWordsController)
 router.use(sourceController)
+router.use(crawledDataController)
 
 //Export a router
 export default router
