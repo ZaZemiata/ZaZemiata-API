113c8ce7a0318b8350beb0398fe069a9d0b331ee
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-11-28 13:43:20 +0200

    Create service for active property

--- src/services/sourceService.ts
+++ src/services/sourceService.ts
@@ -1,19 +1,41 @@
 /**
  * Source Service
- * 
+ *
  * @module source.ts
  * @author vadiim <vadim123bg@gmail.com>
  */
 
-import prisma from '../db/prisma/prisma';
-import { Request, Response } from 'express';
+import prisma from "../db/prisma/prisma";
+import { Request, Response } from "express";
 
 // Get all keywords
 export const getAllSources = async (req: Request, res: Response) => {
-
     // Get all sources from the database
     const sources = await prisma.sources.findMany();
-    
+
     // Send the sources as a response
     res.json(sources);
+};
+
+export const updateSourceActiveStatus = async (id: number, active: boolean) => {
+    try {
+
+        // Attempt to update the 'active' field of the source with the given ID
+        const updatedSource = await prisma.sources.update({
+
+            // Find the source by ID
+            where: { id: id }, 
+            
+            // Update the 'active' field
+            data: { active: active }, 
+        });
+
+        // Return the updated source object
+        return updatedSource;
+
+    } catch (error) {
+
+        // Log the error
+        console.error("Error updating source:", error);
+    }
 };
