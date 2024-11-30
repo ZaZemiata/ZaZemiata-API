050e848cceed1150308762cc65e63c3de3336105
Daniel <danieldimitrov2304@gmail.com>
2024-11-30 12:26:49 +0200

    Conflict resolve

--- src/services/sourceService.ts
+++ src/services/sourceService.ts
@@ -1,7 +1,7 @@
 /**
  * Source Service
- * 
- * @module sourceService.ts
+ *
+ * @module source.ts
  * @author vadiim <vadim123bg@gmail.com>
  */
 
@@ -10,18 +10,32 @@ import { Request, Response } from "express";
 
 // Get all keywords
 export const getAllSources = async (req: Request, res: Response) => {
+    // Get all sources from the database
+    const sources = await prisma.sources.findMany();
+
+    // Send the sources as a response
+    res.json(sources);
+};
+
+export const updateSourceActiveStatus = async (id: number, active: boolean) => {
     try {
+        // Attempt to update the 'active' field of the source with the given ID
+        const updatedSource = await prisma.sources.update({
 
-        // Get all sources from the database
-        const sources = await prisma.sources.findMany();
+            // Find the source by ID
+            where: { id: id },
 
-        // Send the sources as a response
-        res.json(sources);
+            // Update the 'active' field
+            data: { active: active },
+        });
 
+        // Return the updated source object
+        return updatedSource;
     } catch (error) {
+        // Log the error
+        console.error("Error updating source:", error);
 
-        //Catching error and return message
-        throw error
-
+        // Rethrow the error so it can be caught by the controller
+        throw error;
     }
 };
