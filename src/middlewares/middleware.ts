cd750ce171b44577ef5607207925b2368fa76cf4
vadiimXD <vadim123bg@gmail.com>
2024-12-03 18:39:44 +0200

    Created authentication controller and service

--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -1,1 +1,36 @@
-// middware...
+/**
+ * Middlewares
+ *
+ * @module middleware.ts
+ * @author vadiim <vadim123bg@gmail.com>
+ */
+
+//Import dependencies
+import { NextFunction, Request, Response } from "express";
+import config from "../config/config";
+import jwt from "../utils/jwt";
+
+export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        //Get token
+        const token = req.headers["token"]
+
+        //Check token
+        if (!token) {
+            throw Error("Invalid token!")
+        }
+
+
+
+        //Decode token
+        const decodedToken = await jwt.verify(token, config.SECRET);
+        next()
+
+    } catch (error) {
+
+        //Throw error
+        throw Error("Invalid token!")
+
+    }
+
+}
