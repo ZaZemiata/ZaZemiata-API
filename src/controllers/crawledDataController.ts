f3ae92a90e7996772f41d6c6caa0f3258c2cc530
vadiimXD <vadim123bg@gmail.com>
2024-12-05 13:17:34 +0200

    Updated error status for all controllers

--- src/controllers/crawledDataController.ts
+++ src/controllers/crawledDataController.ts
@@ -6,17 +6,30 @@
  */
 
 // Import dependencies
-import express from "express";
+import { Router, Request, Response } from "express";
 import { getAllCrawledData } from "../services/crawledDataService";
+import { ErrorType } from "../types/errorType";
 
 // Create a new router
-const router = express.Router();
+const router = Router();
 
 /**
  * @endpoint GET /api/crawled-data
  * @description Fetch all CrawledData records with related SourceUrls and Sources.
  */
-router.get("/api/crawled-data", getAllCrawledData);
+router.get("/api/crawled-data", async (req: Request, res: Response) => {
+
+    try {
+        // Call the service
+        const data = await getAllCrawledData(req, res)
+
+        // Send response
+        res.status(200).send(data)
+    } catch (error) {
+        // Log the error and respond with an error message
+        res.status(500).send((error as ErrorType).message)
+    }
+});
 
 // Export the router
 export default router;
