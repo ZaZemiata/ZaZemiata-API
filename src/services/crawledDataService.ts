4dc51abeebb44e899f1140ad97c157acc40e434c
batanoffs <batanoff.s@protonmail.com>
2024-12-13 20:53:39 +0200

    add service for pagination

--- src/services/crawledDataService.ts
+++ src/services/crawledDataService.ts
@@ -2,12 +2,14 @@
  * CrawledData Service
  *
  * @module crawledDataService.ts
- * @authowr Hristo Georgiev <hristogeorgiew84@gmail.com>
+ * @author Hristo Georgiev <hristogeorgiew84@gmail.com>
+ * @author Daniel Batanov <batanoff.s@protonmail.com>
  */
 
 import prisma from "../db/prisma/prisma";
 import { Request, Response } from "express";
 import logger from "../utils/logger"; // Import winston logger
+import { PaginationResult } from "../types/pagination";  // Import types
 
 // Get all CrawledData records with related SourceUrls and Sources
 export const getAllCrawledData = async (req: Request, res: Response) => {
@@ -26,5 +28,36 @@ export const getAllCrawledData = async (req: Request, res: Response) => {
 
         // Send a server error message
         res.status(500).json({ message: "Failed to fetch crawled data." });
+    }
+};
+
+// Get limited CrawledData records based on pagination provided in query params
+export const getCrawledDataPagination = async (page: number, limit: number): Promise<PaginationResult> => {
+    try {
+        // Get total records of crawled data
+        const totalEntries = await prisma.crawledData.count();
+
+        // Get data filtered by limit and page
+        const data = await prisma.crawledData.findMany({
+            take: limit,
+            skip: (page - 1) * limit,
+        });
+
+        // Calculate total pages
+        const total = Math.ceil(totalEntries / limit);
+
+        // Log success
+        logger.info("Fetched filtered crawled data successfully.");
+
+        // Return all crawled data
+        return {data, total};
+
+    // Catch errors 
+    } catch (error) {
+        // Log the error
+        logger.error("Error fetching crawled data:", error);
+        
+        // Return error object
+        return {error: (error as Error).message};
     }
 };
