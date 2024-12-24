545da9a3da276ac21006e3332f485d2ac303b120
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-24 12:19:16 +0000

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { ErrorRequestHandler } from 'express';
 import router from './router';
 import { enableBigIntSerialization } from './utils/bigInt';
 import logger from './utils/logger'; // Import winston logger
+import cors from "cors";
 
 // Enable serialization of BigInts
 enableBigIntSerialization();
@@ -17,6 +18,14 @@ app.use((req, res, next) => {
     logger.info(`${req.method} ${req.url}`);
     next();
 });
+
+// CORS
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 // Router
 app.use(router);
