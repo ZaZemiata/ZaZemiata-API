57f020b7e4fcaeacededbb442eff269e54541ef0
Daniel <danieldimitrov2304@gmail.com>
2025-01-21 12:42:08 +0200

    Formatting & comments

--- src/controllers/crawledDataController.ts
+++ src/controllers/crawledDataController.ts
@@ -8,7 +8,7 @@
 
 // Import dependencies
 import { Router, Request, Response } from "express";
-import { getAllCrawledData, getCrawledDataPagination, getCrawledDataWithFilters } from "../services/crawledDataService";
+import { getAllCrawledData, getCrawledDataWithPaginationFilters } from "../services/crawledDataService";
 import { ErrorType } from "../types/errorType";
 import logger from "../utils/logger";
 
@@ -62,13 +62,12 @@ router.get("/api/crawled-data/filter", async (req: Request, res: Response) => {
             containsText: containsText ? String(containsText) : undefined,
         };
 
-        // Call the service with filters
-        const result = await getCrawledDataWithFilters(filters);
+        // Call the service with filters and pagination
+        const result = await getCrawledDataWithPaginationFilters(filters);
 
         // Check if an error occurred
-        if ('error' in result) {
+        if ('error' in result) 
             throw new Error(result.error);
-        }
 
         // Send response with data and pagination info
         res.status(200).send({
@@ -79,16 +78,21 @@ router.get("/api/crawled-data/filter", async (req: Request, res: Response) => {
             },
             data: result.data,
         });
-    } catch (error) {
+    } 
+    
+    // Catch errors
+    catch (error) {
+
+        // Log the error
         logger.error("Error fetching filtered crawled data:", error);
+
+        // Send a bad request response
         res.status(400).send({
             status: 'error',
             message: (error as ErrorType).message,
         });
     }
 });
-
-
 
 // Export the router
 export default router;
